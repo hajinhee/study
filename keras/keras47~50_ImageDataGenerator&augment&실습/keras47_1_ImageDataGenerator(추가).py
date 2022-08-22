82066e56862c4d1284e0691b463dbbc6db6c49a5
hajinhee <hajinheee@gmail.com>
2022-08-22 19:36:10 +0900

    image data generator

--- keras/keras47~50_ImageDataGenerator&augment&실습/keras47_1_ImageDataGenerator(추가).py
+++ keras/keras47~50_ImageDataGenerator&augment&실습/keras47_1_ImageDataGenerator(추가).py
@@ -1,8 +1,10 @@
 import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator     # 대문자니까 class
+from tensorflow.keras.preprocessing.image import ImageDataGenerator    
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 #1. 데이터로드 및 전처리
-
 train_datagen = ImageDataGenerator(    
     rescale=1./255,                    
     horizontal_flip=True,               
@@ -20,24 +22,25 @@ test_datagen = ImageDataGenerator(
 )
 
 xy_train = train_datagen.flow_from_directory(      
-    '../_data/image/brain/train/',
-    target_size = (150, 150),                                                                       
+    'dacon/seoul_landmark/data/train/',
+    target_size=(150, 150),                                                                       
     batch_size=1,                                   
     class_mode='categorical',                         
-    shuffle=True,   seed=66,    # 안의 내용을 섞어줌  randomstate와 마찬가지로 랜덤값고정. 변환할 정도를 랜덤셔플하게 섞어주겠다.
+    shuffle=True,               # 안의 내용을 섞어줌
+    seed=66,                    # randomstate와 마찬가지로 랜덤값 고정. 변환할 정도를 랜덤셔플하게 섞어주겠다.
     color_mode='grayscale',     # 흑백과 컬러를 지정해줄수있다.
-    save_to_dir='../_temp/'     # 건든 데이터를 출력해서보여준다.
+    save_to_dir='../_temp/'     # 데이터를 출력해서 보여준다.
 )   
 
 xy_test = test_datagen.flow_from_directory(         
-    '../_data/image/brain/test/',
-    target_size=(150,150),
+    'dacon/seoul_landmark/data/test/',
+    target_size=(150, 150),
     batch_size=5,
     class_mode='binary',                            
 )  
 
-print(len(xy_train))
-print(xy_train[0])
-print(xy_train[0][0])
-print(xy_train[0][1])
-# print(xy_train[0][0].shape, xy_train[0][1].shape)
+print(len(xy_train))   
+print(xy_train[0])  # 배치사이즈로 1개씩 묶인 xy데이터
+print(xy_train[0][0])  # xy_train의 첫번째 묶음의 첫번째 = x
+print(xy_train[0][1])  # xy_train의 첫번째 묶음의 두번째 = y
+print(xy_train[0][0].shape, xy_train[0][1].shape)
--- keras/keras47~50_ImageDataGenerator&augment&실습/keras47_2_fit_generator.py
+++ keras/keras47~50_ImageDataGenerator&augment&실습/keras47_2_fit_generator.py
@@ -3,27 +3,30 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPool2D
 from tensorflow.keras.callbacks import EarlyStopping
+import os
+import sys
+import matplotlib.pyplot as plt
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-#1. 데이터로드 및 전처리
-
+#1. 데이터 로드 및 전처리
 train_datagen = ImageDataGenerator(    
-    rescale=1./255,                    
-    horizontal_flip=True,               
-    vertical_flip=True,                                      
-    width_shift_range=0.1,            
-    height_shift_range=0.1,   
-    rotation_range=5,               
-    zoom_range=1.2,                 
-    shear_range=0.7,                    
+    rescale=1./255,
+    horizontal_flip=True,  # 상하반전
+    vertical_flip=True,  # 좌우반전
+    width_shift_range=0.1,  # 좌우이동
+    height_shift_range=0.1,  # 상하이동
+    rotation_range=5,  # 회전
+    zoom_range=1.2,  # 부분확대
+    shear_range=0.7,  # 기울기
     fill_mode='nearest'          
 )
 
 test_datagen = ImageDataGenerator(
-    rescale=1./255                      
+    rescale=1./255  # 테스트 데이터는 원본을 사용해야되기 때문에 스케일링만 한다. 
 )
 
 xy_train = train_datagen.flow_from_directory(      
-    '../_data/image/brain/train/',
+    'dacon/seoul_landmark/data/train/',
     target_size = (150, 150),                                                                       
     batch_size=5,                                   
     class_mode='binary',                         
@@ -31,33 +34,36 @@ xy_train = train_datagen.flow_from_directory(
 )   
 
 xy_test = test_datagen.flow_from_directory(         
-    '../_data/image/brain/test/',
-    target_size=(150,150),
+    'dacon/seoul_landmark/data/test/',
+    target_size=(150, 150),
     batch_size=5,
     class_mode='binary',                            
 )  
 
 #2. 모델링
-
 model = Sequential()
-model.add(Conv2D(32, (2,2), padding='same',input_shape=(150,150,3), activation='relu'))
-model.add(MaxPool2D(2))                                                     # 75,75,32
-model.add(Conv2D(16, (4,4), activation='relu'))                            # 72,72,16
+model.add(Conv2D(32, (2, 2), padding='same', input_shape=(150, 150, 3), activation='relu'))
+model.add(MaxPool2D(2))                    
+model.add(Conv2D(16, (4, 4), activation='relu')) 
 model.add(MaxPool2D(4))
 model.add(Flatten())
 model.add(Dense(36, activation='relu'))
 model.add(Dense(18, activation='relu'))
 model.add(Dense(9, activation='relu'))
-model.add(Dense(1,activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
 
-#3. 컴파일,훈련
+#3. 컴파일, 훈련
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-es = EarlyStopping(monitor = "val_acc", patience=50, mode='max',verbose=1,restore_best_weights=True)
-#model.fit(xy_train[0][0], xy_train[0][1])
-hist = model.fit_generator(xy_train,epochs=10000,steps_per_epoch=32, callbacks=[es],# 전체데이터개수 160개를 5배치로 묶어서 32묶음. 이걸 1epoch당 32만큼하겠다? 라고 지정해줌. 확인차의 의미
-                    validation_data=xy_test,
-                    validation_steps=4,             # 이게 뭔지 찾아보자.
-                    )  
+es = EarlyStopping(monitor='val_acc', patience=50, mode='max', verbose=1, restore_best_weights=True)
+hist = model.fit_generator(xy_train, epochs=100, steps_per_epoch=32, callbacks=[es], validation_data=xy_test, validation_steps=4)
+
+'''
+첫번째 인자 : 훈련데이터셋을 제공할 제네레이터를 지정합니다. 본 예제에서는 앞서 생성한 xy_train으로 지정합니다.
+epochs : 전체 훈련 데이터셋에 대해 학습 반복 횟수를 지정합니다. 100번을 반복적으로 학습시켜 보겠습니다.
+steps_per_epoch : 한 epoch에 사용한 스텝 수를 지정합니다. 총 45개의 훈련 샘플이 있고 배치사이즈가 3이므로 15 스텝으로 지정합니다. (트레인 데이터 수 / 배치사이즈)
+validation_data : 검증데이터셋을 제공할 제네레이터를 지정합니다. 본 예제에서는 앞서 생성한 xy_test으로 지정합니다.
+validation_steps : 한 epoch 종료 시 마다 검증할 때 사용되는 검증 스텝 수를 지정합니다. 홍 15개의 검증 샘플이 있고 배치사이즈가 3이므로 5 스텝으로 지정합니다. (테스트 데이터 수 / 배치사이즈)
+'''
 
 acc = hist.history['acc']
 val_acc = hist.history['val_acc']
@@ -69,9 +75,7 @@ print('val_loss : ', val_loss[-51])
 print('acc : ', acc[-51])
 print('val_acc : ', val_acc[-51])
 
-
-import matplotlib.pyplot as plt
-
+# 시각화
 plt.plot(acc, color='red', marker='.',label='acc') 
 plt.plot(val_acc, color='purple', marker='.',label='val_acc') 
 plt.plot(loss, color='green', marker='.',label='loss') 
