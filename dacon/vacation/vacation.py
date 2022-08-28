72ec5c772250bca5aeef4e0d93a647f53418296d
hajinhee <hajinheee@gmail.com>
2022-08-29 03:27:07 +0900

    dacon

--- dacon/vacation/vacation.py
+++ dacon/vacation/vacation.py
@@ -2,20 +2,24 @@ from cProfile import label
 from re import I, X
 import os
 import sys
+from tabnanny import verbose
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import pandas as pd
 from icecream import ic
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier,  VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, VotingClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler,StandardScaler,RobustScaler,MaxAbsScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedKFold  # 회귀: KFold, 분류: StratifiedKFold
 from sklearn.model_selection import cross_val_score
-from tensorflow.keras.utils import to_categorical 
 from sklearn.model_selection import GridSearchCV
+# from xgboost import XGBClassifier, XGBRegressor
+from catboost import CatBoostRegressor, CatBoostClassifier
+from bayes_opt import BayesianOptimization
+from sklearn.metrics import matthews_corrcoef
 from sklearn.impute import SimpleImputer
 
 # 데이터 준비
@@ -79,7 +83,7 @@ for these in [train, test]:
 
 # 이상치 제거
 # ic(train['NumberOfFollowups'].sort_values(ascending=False), '\n')
-# ic(train.iloc[[987]])
+# ic(train.iloc[[189]])
 train.drop(index=[189, 604, 1338, 987], inplace=True)
 
 # 결측치 처리
@@ -90,6 +94,7 @@ for these in [train, test]:
 
 for these in [train, test]:
         these['PreferredPropertyStar'].fillna(these.groupby('NumberOfTrips')['PreferredPropertyStar'].transform('mean'), inplace=True)
+        these['NumberOfTrips'].fillna(these.groupby('DurationOfPitch')['NumberOfTrips'].transform('mean'), inplace=True)
         these['Age'].fillna(these.groupby('Designation')['Age'].transform('mean'), inplace=True)
 
 # 스케일
@@ -102,40 +107,89 @@ test[['Age', 'DurationOfPitch']] = scaler.transform(test[['Age', 'DurationOfPitc
 # ic(train.describe())  
 
 # train을 x, y로 나누고 불필요한 컬럼 제거
-train.drop(columns=['id', 'NumberOfChildrenVisiting', 'NumberOfPersonVisiting', 'OwnCar', 'NumberOfTrips', 'MonthlyIncome', 'NumberOfFollowups'], axis=1, inplace=True)
-test.drop(columns=['id', 'NumberOfChildrenVisiting', 'NumberOfPersonVisiting', 'OwnCar', 'NumberOfTrips', 'MonthlyIncome', 'NumberOfFollowups'], axis=1, inplace=True)
+train.drop(columns=['id', 'NumberOfChildrenVisiting', 'NumberOfPersonVisiting', 'OwnCar', 'MonthlyIncome', 'NumberOfFollowups'], axis=1, inplace=True)
+test.drop(columns=['id', 'NumberOfChildrenVisiting', 'NumberOfPersonVisiting', 'OwnCar', 'MonthlyIncome', 'NumberOfFollowups'], axis=1, inplace=True)
 x = train.drop(columns=['ProdTaken'], axis=1)
 y = train[['ProdTaken']]
 
-k_fold = KFold(n_splits=10, shuffle=True, random_state=66)
+k_fold = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)  # StratifiedKFold --> 분류문제에 사용
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.9, shuffle=True, random_state=1234, stratify=y)  # stratify=y -> y개수만큼 컷
 
-rf = ExtraTreesClassifier(n_jobs=-1)
-params = {
-    'n_estimators' : (100, 150, 200, 250, 300, 400, 450, 500, 550, 600, 650, 700, 750, 800)
+# BayesianOptimization
+# def CB_opt(n_estimators, depth, learning_rate, 
+#             l2_leaf_reg, model_size_reg, od_pval): 
+#     scores = []
+#     # skf = k_fold
+# #   for train_index, test_index in skf.split(x, y):
+    
+# #     x_train, x_val = x.iloc[train_index], x.iloc[test_index]
+# #     y_train, y_val = y.iloc[train_index], y.iloc[test_index]
+ 
+#     reg = CatBoostClassifier(verbose=0,
+#                             n_estimators=int(n_estimators),
+#                             learning_rate=learning_rate,
+#                             l2_leaf_reg=l2_leaf_reg,
+#                             max_depth=int(depth),
+#                             random_state=42,
+#                             grow_policy='Lossguide',
+#                             use_best_model=True, 
+#                             model_size_reg=model_size_reg,
+#                             od_pval=od_pval
+#                             )
+#     reg.fit(x_train, y_train, eval_set=(x_test, y_test))
+#     scores.append(matthews_corrcoef(y_test, reg.predict(x_test)))
+#     return np.mean(scores)
+
+# pbounds = {'n_estimators': (150, 1000),
+#            'depth': (4, 12),
+#            'learning_rate': (.01, 0.3),
+#            'l2_leaf_reg': (0, 10),
+#            'model_size_reg': (0, 10),
+#            'od_pval' : (0, 5)
+# }
+# optimizer = BayesianOptimization(
+#     f=CB_opt,
+#     pbounds=pbounds,
+#     verbose=0,
+#     random_state=42,
+# )
+# optimizer.maximize(init_points=2, n_iter=20)
+# ic(optimizer.max)
+
+
+# rf_params = {
+#     'n_estimators' : [100, 150, 200, 250, 300, 400, 450, 500, 550, 600, 650, 700, 750, 800]
+# }
+# rf = ExtraTreesClassifier(n_jobs=-1)
+
+cat_params = {
+    # 'depth': [10, 11, 14, 15],
+    'l2_leaf_reg': [9.122611898980937],    
+    'learning_rate': [0.279118773316628],
+    'model_size_reg': [5.669787571831169],
+    # 'n_estimators': [200, 300, 400, 500, 600],
+    'od_pval': [0.0],
 }
-
-grid_cv = GridSearchCV(rf,
-                       param_grid=params,
-                       cv = k_fold,
+                             
+cat = CatBoostClassifier(verbose=False, depth=14, n_estimators=200)  # 데이터에 범주형 변수가 많을 경우 유용
+grid_cv = GridSearchCV(cat,
+                       param_grid=cat_params,
+                       cv=k_fold,
                        n_jobs=-1)
+
 grid_cv.fit(x, y.values.ravel())
-ic(grid_cv.best_estimator_)
+ic(grid_cv.best_estimator_, grid_cv.best_params_, grid_cv.best_score_)  # best_score_: 최고 점수, best_params_: 최고 점수를 낸 파라미터, best_estimator_: 최고 점수를 낸 파라미터를 가진 모형
 
 model = grid_cv.best_estimator_
-model.fit(x, y.values.ravel())
+# model.fit(x, y.values.ravel())
 
-# model.fit(x_train, y_train.values.ravel())
-# ic('Train Accuracy : {:.2f}'.format(model.score(x_train, y_train)))
-# ic('Test Accuracy : {:.2f}'.format(model.score(x_test, y_test)))
-# y_pred = model.predict(x_test)
-# ic('score:', accuracy_score(y_pred, y_test)) 
+model.fit(x_train, y_train.values.ravel())
+y_pred = model.predict(x_test)
+ic('score:', accuracy_score(y_pred, y_test)) 
 
+# KFold 교차검증
 score = cross_val_score(model, x, y.values.ravel(), cv=k_fold, n_jobs=-1, scoring='accuracy')
 ic(score, np.mean(score)) 
-# KFold 교차검증
-# k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
-# score = cross_val_score(RandomForestClassifier(), x, y, cv=k_fold, n_jobs=1, scoring='accuracy')
-# ic('k_fold_score:', score) 
 
 # 데이터 submit
 y_summit = model.predict(test)
--- keras/keras55~59_autokeras&LR&GlobalAverage/keras55_1_hyperParameter.py
+++ keras/keras55~59_autokeras&LR&GlobalAverage/keras55_1_hyperParameter.py
@@ -3,21 +3,26 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, Input
-
-#1. 데이터
-
-(x_train, y_train), (x_test,y_test) = mnist.load_data()
-
-x_train = x_train.reshape(60000, 28*28).astype('float32')/255
-x_test = x_test.reshape(10000, 28*28).astype('float32')/255          # 정수형 -> 실수형으로 바꿔줌 + Minmax + 2차원데이터로 바꿔서 Dense하겠다
-
 from tensorflow.keras.utils import to_categorical
+from icecream import ic
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor 
+import time
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+import os
 
+#1. 데이터 로드 및 정제
+(x_train, y_train), (x_test,y_test) = mnist.load_data()
+x_train = x_train.reshape(60000, 28*28).astype('float32')/255   # int -> float, Minmax, 3d -> 2d
+x_test = x_test.reshape(10000, 28*28).astype('float32')/255         
+ic(np.unique(y_train, return_counts=True))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] -> 다중분류
+
+# 원핫인코딩
 y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)                                     # 오랜만에 해주는 원핫인코딩. 이게 싫으면 loss에서 sparse_categorical_crossentropy 해주면된다.
+y_test = to_categorical(y_test)
+ic(len(y_train))  # 60000
 
-#2. 모델
-
+#2. 모델링
 def build_model(drop=0.5, optimizer='adam', activation='relu'):
     inputs = Input(shape=(28*28), name='input')
     x = Dense(512, activation=activation, name='hidden1')(inputs)
@@ -26,34 +31,24 @@ def build_model(drop=0.5, optimizer='adam', activation='relu'):
     x = Dropout(drop)(x)
     x = Dense(128, activation=activation, name='hidden3')(x)
     x = Dropout(drop)(x)
-    outputs = Dense(10,activation='softmax',name='outputs')(x)
-    
+    outputs = Dense(10, activation='softmax', name='outputs')(x)
     model = Model(inputs=inputs,outputs=outputs)
-    
-    model.compile(optimizer=optimizer, metrics=['acc'],
-                  loss='categorical_crossentropy')
-    
+    model.compile(optimizer=optimizer, metrics=['acc'], loss='categorical_crossentropy')
     return model
 
 def create_hyperparameter():
-    batchs = [100,200,300,400,500]
-    optimizers = ['adam', 'rmsprop', 'adadelta']                # 그라디언트 폭발과 소멸 optimizers를 진짜 잘 조정해야 튜닝의 고수다.
+    batchs = [32, 64, 128, 256, 512]
+    optimizers = ['adam', 'rmsprop', 'adadelta']
     dropout = [0.3, 0.4, 0.5]
-    activation = ['relu','linear','sigmoid','selu','elu']
-    return {"batch_size" : batchs, "optimizer" : optimizers, "drop" : dropout,
-            "activation" : activation}
+    activation = ['relu', 'linear', 'sigmoid', 'selu', 'elu']
+    return {'batch_size' : batchs, 'optimizer' : optimizers, 'drop' : dropout, 'activation' : activation}
 
 hyperparameters = create_hyperparameter()
-
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier,KerasRegressor   # KerasClassifier,KerasRegressor 2가지형태가있다.
-keras_model = KerasClassifier(build_fn=build_model, verbose=1)  # tensorflow를 scikit_learn형태로 감싸준다.  
-
-from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
+keras_model = KerasClassifier(build_fn=build_model, verbose=1)  
 model = GridSearchCV(keras_model, hyperparameters, cv=3, verbose=1)
 
-import time
 start = time.time()
-model.fit(x_train,y_train, verbose=1, epochs=30, validation_split=0.2)
+model.fit(x_train, y_train, verbose=1, epochs=30, validation_split=0.2)
 #estimator should be an estimator implementing 'fit' method, <function build_model at 0x000001FD710C9160> was passed
 #GridSearchCV는 머신러닝에서 썼었고 지금은 딥러닝이다. 서로 짝을 맞춰줘야한다.
 end = time.time()
@@ -64,12 +59,10 @@ print("model.best_estimator_",model.best_estimator_)
 print("model.best_score_",model.best_score_)
 print("model.score",model.score)
 
-from sklearn.metrics import accuracy_score
 y_predict = model.predict(x_test)
 print("accuracy_score : ", accuracy_score(y_test,y_predict))
 
 # 가중치 save
-import os
 path = os.getcwd() + '/'
 model.save(path + "keras55_1_save_model.h5")
 model.save_weights(path + "keras55_1_save_weights.h5")
