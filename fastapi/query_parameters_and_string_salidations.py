7da7b3538601e76c17ca2a86f65b1f896b8b425d
hajinhee <hajinheee@gmail.com>
2022-10-18 22:25:51 +0900

    fastapi

--- fastapi/query_parameters_and_string_salidations.py
+++ fastapi/query_parameters_and_string_salidations.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 from fastapi import FastAPI, Query
 from pydantic import Required
 
@@ -25,21 +25,66 @@ app = FastAPI()
 #     return results
 
 # Required with Ellipsis (...)
-@app.get('/items/')
-# async def read_items(q: str = Query(default=..., min_length=3)):
-async def read_items(q: Union[str, None] = Query(default=..., min_length=3)):
-    results = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
-    if q:
-        results.update({'q': q})  # type: ignore
-    return results
-
+# @app.get('/items/')
+# # async def read_items(q: str = Query(default=..., min_length=3)):
+# async def read_items(q: Union[str, None] = Query(default=..., min_length=3)):
+#     results = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
+#     if q:
+#         results.update({'q': q})  # type: ignore
+#     return results
 
 # Use Pydantic's Required instead of Ellipsis (...)
+# @app.get("/items/")
+# async def read_items2(q: str = Query(default=Required, min_length=3)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})  # type: ignore
+#     return results
+
+# Query parameter list / multiple values
+# @app.get('/items/')
+# async def read_items3(q: Union[List[str], None] = Query(default=None)):
+# async def read_items3(q: List[str] = Query(default=["foo", "bar"])):
+# async def read_items3(q: list = Query(default=[])):
+#     query_items = {'q' : q}
+#     return query_items
+
+
+# Declare more metadata
+# @app.get("/items/")
+# async def read_items(
+#     q: Union[str, None] = Query(default=None, title="Query string", min_length=3)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})  # type: ignore
+#     return results
+
+# @app.get("/items/")
+# async def read_items(
+#     q: Union[str, None] = Query(
+#         default=None,
+#         title="Query string",
+#         description="Query string for the items to search in the database that have a good match",
+#         min_length=3,)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})  # type: ignore
+#     return results
+
+# Deprecating parameters
 @app.get("/items/")
-async def read_items2(q: str = Query(default=Required, min_length=3)):
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        regex="^fixedquery$",
+        deprecated=True)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})  # type: ignore
     return results
 
-# Query parameter list / multiple values
