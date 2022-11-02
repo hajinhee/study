71d3e299ba1fe6eb128a2712c5f2b5b33084b174
hajinhee <hajinheee@gmail.com>
2022-11-03 01:18:03 +0900

    js

--- js/slide.js
+++ js/slide.js
@@ -6,27 +6,55 @@ const backBtn = document.querySelector(".back")
 const nextBtn = document.querySelector(".next")
 const images = document.querySelector(".images")
 
+window.onload = function(){
+    makedots(imgCount);
+}
+
+function makedots(imgCount){
+    for(let i=0; i<=imgCount; i++){
+        dot_name = "dot a"+i;
+        document.getElementById("dot").innerHTML += `<div class="${dot_name}" id="${dot_name}"></div>`
+        if(i==0)
+            document.getElementById("dot a"+i).style= "background:#40E0D0; margin-left:"+(15*i)+"px";
+        else
+            document.getElementById(dot_name).style= "background:#DCDCDC; margin-left:"+(15*i)+"px";
+    }
+}
+
+function change_color(pages){
+    for(let i=0; i<=imgCount; i++){
+        if(i==pages)
+            document.getElementById("dot a"+i).style= "background:#40E0D0; margin-left:"+(15*i)+"px";
+        else
+            document.getElementById("dot a"+i).style= "background:#DCDCDC; margin-left:"+(15*i)+"px";
+    }
+}
+
 function next() {
-  if (pages< imgCount) {
-    backBtn.removeAttribute('disabled')  // 뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
+  if (pages < imgCount) {
+    backBtn.removeAttribute('disabled')  
     positionValue -= IMAGE_WIDTH;  // IMAGE_WIDTH의 증감을 positionValue에 저장한다.
-    images.style.transform = `translateX(${positionValue}px)`;  // x축으로 positionValue만큼의 px을 이동한다.
+    images.style.transform = `translateX(${positionValue}px)`;  
     pages += 1;  // 다음 페이지로 이동해서 pages를 1증가 시킨다.
+    change_color(pages)
   }
   if (pages === imgCount) { 
-    nextBtn.setAttribute('disabled', 'true')  // 마지막 장일 때 next버튼이 disabled된다.
+    nextBtn.setAttribute("disabled", "true");
+
   }
+    // 마지막 장일 때 next버튼이 disabled된다.
 }
 
 function back() {
   if (pages > 0) {
     nextBtn.removeAttribute('disabled')
     positionValue += IMAGE_WIDTH;
-    images.style.transform = `translateX(${positionValue}px)`;
+    images.style.transform = `translateX(${positionValue}px)`;  // transform 속성의 값으로 사용되며 요소를 x축으로 이동할 수 있다.
     pages -= 1;  //이전 페이지로 이동해서 pages를 1감소 시킨다.
+    change_color(pages)
   }
   if (pages === 0) {
-    backBtn.setAttribute('disabled', 'true')  //마지막 장일 때 back버튼이 disabled된다.
+    backBtn.setAttribute("disabled", "true"); //첫번째 장일 때 back버튼이 disabled된다.
   }
 }
 
@@ -37,3 +65,11 @@ function init() {  //초기 화면 상태
 }
 init();
 
+
+document.getElementById("modal_up").addEventListener('click', () => {
+    document.getElementById("auth_modal").style.display = "flex";
+});
+
+document.getElementById("btn-close").addEventListener('click', () => {
+    document.getElementById("auth_modal").style.display = "none";
+})
